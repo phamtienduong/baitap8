a08fc27bcfede3bc606503a640739df26b2857b6
tien duong <phamtienduong99@gmail.com>
2023-08-21 13:33:54 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+ console.log("wellcome to Rikkei Academy");
+ prompt("mời nhập số tuổi của mình");
+ alert("Quyết tâm học lập trình javascript")
+ confirm("Quyết tâm học lập trình javascript")
+let physics= +prompt("điểm môn Vật Lý");
+ let chemistry= +prompt("điểm môn Hoá Học");
+ let biology= +prompt("điểm môn Sinh Học");
+ let tong=(physics+chemistry+biology);
+ console.log("Tổng 3 môn", tong);
+ let C= +prompt("nhập độ C ở đây");
+ let F= (9*C)/5+32;
+console.log(F);
+let radius= +prompt ("nhập bán kính ở đây")
+const PI=3.14;
+let result= radius*radius*PI;
+ console.log(`diện tích hình tròn có bán kính ${radius} nà :`, result);
+ let result= 5>7 || 6>7 || 8>7
+console.log(result);
+let a= null ?? undefined ?? 6;
+console.log(a);
+let a=9;
+ console.log("🚀 ~ file: operator.js:27 ~ a:", a)
+let result= 5 > "a" ;
+console.log(result);
